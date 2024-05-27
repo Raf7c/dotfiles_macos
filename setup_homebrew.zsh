445ae81d3f12b4f916b15f7ae8b85dfc8b40b21a
Raphael C <122828688+Raf7c@users.noreply.github.com>
2024-05-28 01:06:26 +0200

    start config setup homebrew

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,1 +1,12 @@
 #!/usr/bin/env zsh
+
+
+echo "\n<<< Starting Homebrew Setup >>>\n"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install bat
+
+brew install --no-quarantine --cask visual-studio-code
+brew install --no-quarantine --cask raycast
+brew install --no-quarantine --cask kitty
