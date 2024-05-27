654dd59ed34f837414286772450affb53b1fc3d0
Raphael C <122828688+Raf7c@users.noreply.github.com>
2024-05-28 01:27:00 +0200

    Switch Brewfile

---
+++ Brewfile
@@ -0,0 +1,11 @@
+# Taps
+tap "homebrew/bundle"
+
+# Brew (Packages)
+brew "bat"
+
+# Cask (Applications)
+cask "kitty"
+cask "raycast"
+cask "visual-studio-code"
+vscode "ms-ceintl.vscode-language-pack-fr"
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -5,8 +5,4 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install bat
-
-brew install --no-quarantine --cask visual-studio-code
-brew install --no-quarantine --cask raycast
-brew install --no-quarantine --cask kitty
+brew bundle --verbose
